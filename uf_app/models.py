2a3e1d3dcbb03b559ad0795fbdf8fb785a7240e4
Luis Boscan <luife642@gmail.com>
2017-11-26 14:38:55 -0300

    UFValue model & tests added

--- uf_app/models.py
+++ uf_app/models.py
@@ -1,3 +1,22 @@
+import pendulum
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import ugettext as _
+
+
+class UFValue(models.Model):
+    value = models.FloatField(verbose_name=_('Value in Chilean pesos'))
+    date = models.DateField(verbose_name=_('Value date'))
+
+    def __str__(self):
+        date = pendulum.date.instance(self.date)
+        return '{}: {} CLP.'.format(
+            date.to_formatted_date_string(),
+            self.value
+        )
+
+    class Meta:
+        verbose_name = _('uf value')
+        verbose_name_plural = _('uf values')
+
+        unique_together = (('value', 'date'),)
---
+++ uf_app/tests/test_models.py
@@ -0,0 +1,51 @@
+import pendulum
+from django.db import IntegrityError
+from django.test import TestCase
+
+from uf_app.models import UFValue
+
+
+class UFValueModelTest(TestCase):
+    def test_cannot_save_empty_uf_value(self):
+        with self.assertRaises(IntegrityError):
+            UFValue.objects.create()
+
+    def test_create_valid_uf_value(self):
+        value = 26348.83
+        date = pendulum.date.create(2017, 1, 1)
+
+        uf_value = UFValue.objects.create(
+            value=value,
+            date=date
+        )
+
+        self.assertEqual(value, uf_value.value)
+        self.assertEqual(date, uf_value.date)
+
+    def test_cannot_create_equal_values(self):
+        value = 26348.83
+        date = pendulum.date.create(2017, 1, 1)
+
+        with self.assertRaises(IntegrityError):
+            uf_values = [
+                UFValue(value=value, date=date),
+                UFValue(value=value, date=date)
+            ]
+
+            UFValue.objects.bulk_create(uf_values)
+
+    def test_can_create_different_values(self):
+        value1 = 26348.83
+        date1 = pendulum.date.create(2017, 1, 1)
+
+        value2 = 26349.68
+        date2 = pendulum.date.create(2017, 1, 2)
+
+        uf_values = [
+            UFValue(value=value1, date=date1),
+            UFValue(value=value2, date=date2)
+        ]
+
+        uf_objects = UFValue.objects.bulk_create(uf_values)
+
+        self.assertEqual(len(uf_objects), 2)
