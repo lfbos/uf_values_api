063ccf35d530ed1cb43148aa98d73e7b1665bb96
Luis Boscan <luife642@gmail.com>
2017-11-28 20:43:10 -0300

    Spider modified

--- uf_app/uf_values_scraper/uf_values_scraper/spiders/uf_values_spider.py
+++ uf_app/uf_values_scraper/uf_values_scraper/spiders/uf_values_spider.py
@@ -26,25 +26,32 @@ class UFValuesSpider(scrapy.Spider):
     uf_values = []
 
     def parse(self, response):
-        #  First iteration always will be current year
+        #  Tag to parse only current year
         only_current_year = getattr(self, 'only_current_year', False)
 
+        #  Get all the years with data available
         years = response.css('#DrDwnFechas option::text').extract()
 
+        #  First iteration always will be current year
         selected_year = response.css(
             '#DrDwnFechas option[selected="selected"]::text'
         ).extract_first()
 
+        #  Save next year
         next_year = str(int(selected_year) - 1)
 
+        #  Save last year
         last_year = years[0]
 
         day = None
         month = None
 
-        current_uf_values = UFValue.objects.values_list('value', 'date')
+        #  Get saved values
+        current_uf_values = UFValue.objects.values_list('date')
 
+        #  Iterate over all columns
         for td in response.css('.Grid tr td'):
+            #  Extract text
             text = td.css('::text').extract_first()
 
             if text in MONTH_DAYS:  # If cell is day of month
@@ -54,6 +61,8 @@ class UFValuesSpider(scrapy.Spider):
             else:
                 value = text  # UF Value
 
+                #  Parse the year
+                #  Prevent exception parsing date values
                 try:
                     date = pendulum.date.create(
                         int(selected_year),
@@ -64,22 +73,33 @@ class UFValuesSpider(scrapy.Spider):
                     date = None
 
                 if value is not None and date is not None:
+                    #  Change uf value format
                     value = float(value.replace('.', '').replace(',', '.'))
 
-                    if (value, date) not in current_uf_values:
+                    #  If date not in current values
+                    if date not in current_uf_values:
+                        #  Insert new uf value
                         self.uf_values.append(
                             UFValue(
                                 value=value,
                                 date=date
                             )
                         )
-                        yield {
-                            "value": value,
-                            "date": date
-                        }
+                    elif only_current_year:
+                        #  Check values for current year (some probably changed)
+                        UFValue.objects.filter(
+                            date=date  # Filter by date
+                        ).exclude(
+                            value=value  # Exclude the value if is equal than before
+                        ).update(
+                            value=value  # Update
+                        )
 
                 month += 1  # Continue to the next month
 
+        # If next year is less greater or equal than last year
+        # And is not only current year
+        # Keep parsing
         if int(next_year) >= int(last_year) and not only_current_year:
             yield FormRequest.from_response(
                 response,
