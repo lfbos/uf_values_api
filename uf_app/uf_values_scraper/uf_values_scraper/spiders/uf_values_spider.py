d94e05d671f9d68f514825f676569c2772da2c7e
Luis Felipe Boscan <luife642@gmail.com>
2017-11-30 16:00:55 -0300

    Spider code modified and util method added

--- uf_app/uf_values_scraper/uf_values_scraper/spiders/uf_values_spider.py
+++ uf_app/uf_values_scraper/uf_values_scraper/spiders/uf_values_spider.py
@@ -1,11 +1,11 @@
 import logging
 from decimal import Decimal
 
-import pendulum
 import scrapy
 from scrapy import FormRequest
 
 from uf_app.models import UFValue
+from uf_app.utils import get_valid_date
 
 MONTH_DAYS = list(map(str, range(1, 32)))
 
@@ -27,103 +27,28 @@ class UFValuesSpider(scrapy.Spider):
     uf_values = []
 
     def parse(self, response):
-        #  Tag to parse only current year
         only_current_year = getattr(self, 'only_current_year', False)
 
-        #  Get all the years with data available
         years = response.css('#DrDwnFechas option::text').extract()
 
-        #  First iteration always will be current year
         selected_year = response.css(
             '#DrDwnFechas option[selected="selected"]::text'
         ).extract_first()
 
-        #  Save next year
-        next_year = str(int(selected_year) - 1)
+        previous_year = str(int(selected_year) - 1)
 
-        #  Save last year
         last_year = years[0]
 
-        day = None
-        month = None
-
-        #  Get saved values
         current_uf_values = UFValue.objects.values('value', 'date')
-        current_dates = list(map(lambda uf_value: uf_value.get('date'), current_uf_values))
 
-        #  Iterate over all columns
-        for td in response.css('.Grid tr td'):
-            #  Extract text
-            text = td.css('::text').extract_first()
+        self._iterate_and_validate_columns(selected_year, current_uf_values, response)
 
-            if text in MONTH_DAYS:  # If cell is day of month
-                day = int(text)  # Saved it
-                month = 1  # Reboot year
-                continue  # Continue in the next iteration
-            else:
-                value = text  # UF Value
-
-                #  Parse the year
-                #  Prevent exception parsing date values
-                try:
-                    date = pendulum.date.create(
-                        int(selected_year),
-                        month,
-                        day
-                    )
-                except ValueError:
-                    date = None
-
-                if value is not None and date is not None:
-                    #  Change uf value format
-                    value = Decimal(value.replace('.', '').replace(',', '.'))
-
-                    #  If date not in current dates
-                    if date not in current_dates:
-                        #  Insert new uf value
-                        self.uf_values.append(
-                            UFValue(
-                                value=value,
-                                date=date
-                            )
-                        )
-                    elif only_current_year:
-
-                        #  Check values for current year (some probably changed)
-                        uf = list(
-                            filter(
-                                lambda uf_value: uf_value.get('date') == date,
-                                current_uf_values
-                            )
-                        )[0]
-
-                        current_value = uf.get('value')
-
-                        if value != current_value:  # If value is different
-                            UFValue.objects.filter(
-                                date=date  # Filter by date
-                            ).update(
-                                value=value  # Update
-                            )
-
-                            logger.info(
-                                'Value for the date {} updated: {}'.format(
-                                    date,
-                                    value
-                                )
-                            )
-
-                month += 1  # Continue to the next month
-
-        # If next year is less greater or equal than last year
-        # And is not only current year
-        # Keep parsing
-        if int(next_year) >= int(last_year) and not only_current_year:
+        if int(previous_year) >= int(last_year) and not only_current_year:
             yield FormRequest.from_response(
                 response,
                 formname="form1",
                 formdata={
-                    "DrDwnFechas": next_year
+                    "DrDwnFechas": previous_year
                 }
             )
 
@@ -135,3 +60,59 @@ class UFValuesSpider(scrapy.Spider):
                     len(uf_values_created)
                 )
             )
+
+    def _iterate_and_validate_columns(self, selected_year, current_uf_values, response):
+        only_current_year = getattr(self, 'only_current_year', False)
+
+        day = None
+        month = None
+
+        current_dates = list(map(lambda uf_value: uf_value.get('date'), current_uf_values))
+
+        for td in response.css('.Grid tr td'):
+            cell = td.css('::text').extract_first()
+
+            if cell in MONTH_DAYS:
+                day = int(cell)  # Day of month
+                month = 1
+                continue
+            else:
+                uf_value = cell  # UF Value
+
+                date = get_valid_date(int(selected_year), month, day)
+
+                if uf_value is not None and date is not None:
+                    uf_value = Decimal(uf_value.replace('.', '').replace(',', '.'))
+
+                    if date not in current_dates:
+                        self.uf_values.append(
+                            UFValue(
+                                value=uf_value,
+                                date=date
+                            )
+                        )
+                    elif only_current_year:
+                        uf = list(
+                            filter(
+                                lambda uf_value: uf_value.get('date') == date,
+                                current_uf_values
+                            )
+                        )[0]
+
+                        current_value = uf.get('value')
+
+                        if uf_value != current_value:
+                            UFValue.objects.filter(
+                                date=date
+                            ).update(
+                                value=uf_value
+                            )
+
+                            logger.info(
+                                'Value for the date {} updated: {}'.format(
+                                    date,
+                                    uf_value
+                                )
+                            )
+
+                month += 1
--- uf_app/utils.py
+++ uf_app/utils.py
@@ -1,2 +1,20 @@
+import pendulum
+
+
 def pesos_to_uf(pesos, uf_value):
     return pesos / uf_value
+
+
+def get_valid_date(year, month, day):
+    date = None
+
+    try:
+        date = pendulum.date.create(
+            year,
+            month,
+            day
+        )
+    except ValueError:
+        pass
+
+    return date
