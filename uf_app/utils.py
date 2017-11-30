8e9a8e37f91060b6c42a50de88d12886e3239a6d
Luis Felipe Boscan <luife642@gmail.com>
2017-11-30 15:07:28 -0300

    unused method removed

--- uf_app/utils.py
+++ uf_app/utils.py
@@ -1,13 +1,2 @@
-import pendulum
-
-
-def string_to_date(date_string):
-    year = int(date_string[:4])
-    month = int(date_string[4:6])
-    day = int(date_string[6:])
-
-    return pendulum.date.create(year, month, day)
-
-
 def pesos_to_uf(pesos, uf_value):
     return pesos / uf_value
